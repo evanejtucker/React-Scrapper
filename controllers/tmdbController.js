68995f12f8b4d5a13782bb328d8dc716836de223
Evan Tucker <eej.tucker@gmail.com>
2018-12-08 11:40:57 -0700

    added to tmdb controller

--- controllers/tmdbController.js
+++ controllers/tmdbController.js
@@ -1,12 +1,20 @@
 const db = require("../models");
 const keys = require("../keys");
 const axios = require("axios");
+const API = process.env.TMDB_API || keys.moviesDB.API_Key;
 
 module.exports = {
     findUpcoming: (req, res)=> {
-        let API = process.env.TMDB_API || keys.moviesDB.API_Key;
-        let url = "https://api.themoviedb.org/3/discover/movie?api_key=" + API + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
-        axios.get(url).then((movies)=> {
+        // let API = process.env.TMDB_API || keys.moviesDB.API_Key;
+        let url = "https://api.themoviedb.org/3/discover/movie?";
+        axios.get(url, {params: {
+            api_key: API,
+            language: "en-US",
+            // sort_by: "release_date.desc",
+            include_adult: false,
+            include_video: false,
+            page: 1
+        }}).then((movies)=> {
             let movieArray = [];
             for (let i=0; i<movies.data.results.length; i++) {
                 let movie = {
@@ -18,13 +26,34 @@ module.exports = {
                 }
                 movieArray.push(movie);  
             }
-            db.Movie.insertMany(movieArray, function (err, response) {
-                if (err) return handleError(err);
-                console.log("movies successfully added to the db");
-            });
+            // db.Movie.insertMany(movieArray, function (err, response) {
+            //     if (err) return handleError(err);
+            //     console.log("movies successfully added to the db");
+            // });
             res.json(movieArray);
         }).catch((err)=> {
             res.send(err);
         });
-    }
+    },
+
+    searchMovie: (req, res)=> {
+        // console.log(req.body.params)
+        console.log('getting into route');
+        url = "https://api.themoviedb.org/3/search/movie?"
+        data = {
+            api_key: API,
+            language: "en-US",
+            query: req.params.movie,
+            page: 1,
+            include_adult: false
+        }
+        axios.get(url, {
+            params: data
+        }).then((response)=> {
+            console.log(response.data.results);
+            res.json(response.data.results);
+        }).catch((err)=> {
+            res.json(err);
+        });
+    } 
 }
--- routes/api/tmdb.js
+++ routes/api/tmdb.js
@@ -3,7 +3,11 @@ const tmdbController = require("../../controllers/tmdbController");
 
 // Matches with "/api/tmdb/upcoming"
 router
-  .route("/upcoming")
+  .route("/search/upcoming")
   .get(tmdbController.findUpcoming);
+
+router
+  .route("/search/movie/:movie")
+  .get(tmdbController.searchMovie)
 
 module.exports = router;
