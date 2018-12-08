fa626a40a90ff7d04caf22f8effb851f7094d890
Evan Tucker <eej.tucker@gmail.com>
2018-12-08 14:04:03 -0700

    worked ontmdb controller :snail:

--- controllers/tmdbController.js
+++ controllers/tmdbController.js
@@ -4,13 +4,15 @@ const axios = require("axios");
 const API = process.env.TMDB_API || keys.moviesDB.API_Key;
 
 module.exports = {
+
+    // find list of upcoming movies
     findUpcoming: (req, res)=> {
         // let API = process.env.TMDB_API || keys.moviesDB.API_Key;
         let url = "https://api.themoviedb.org/3/discover/movie?";
         axios.get(url, {params: {
             api_key: API,
             language: "en-US",
-            // sort_by: "release_date.desc",
+            sort_by: "release_date.desc",
             include_adult: false,
             include_video: false,
             page: 1
@@ -30,30 +32,79 @@ module.exports = {
             //     if (err) return handleError(err);
             //     console.log("movies successfully added to the db");
             // });
+            console.log("movies successfully loaded!");
             res.json(movieArray);
         }).catch((err)=> {
+            console.log("something went wrong :(");
             res.send(err);
         });
     },
 
-    searchMovie: (req, res)=> {
-        // console.log(req.body.params)
-        console.log('getting into route');
-        url = "https://api.themoviedb.org/3/search/movie?"
-        data = {
+    // searches for a specific movie or TV show
+    searchTitle: (req, res)=> {
+        let url = `https://api.themoviedb.org/3/search/${req.params.searchType}?`
+        let data = {
             api_key: API,
             language: "en-US",
-            query: req.params.movie,
+            query: req.params.title,
             page: 1,
             include_adult: false
         }
         axios.get(url, {
             params: data
         }).then((response)=> {
+            console.log("response successfully loaded!");
+            res.json(response.data.results);
+        }).catch((err)=> {
+            console.log("Something went wrong :(");
+            res.json(err);
+        });
+    }, 
+
+    // searchTitleDetails: (req, res)=> {
+    //     let url = `https://api.themoviedb.org/3/find/`
+    //     let data = {
+    //         api_key: API,
+    //         language: "en-US",
+    //         external_id: req.params.titleID,
+    //         page: 1,
+    //         include_adult: false
+    //     }
+    //     axios.get(url, {
+    //         params: data
+    //     }).then((response)=>{
+    //         console.log(response.data.results);
+    //         res.json(response.data.results);
+    //     }).catch((err)=> {
+    //         res.json(err);
+    //     });
+    // },
+
+    movieDetails: (req, res)=> {
+        console.log("function hit");
+        let url = `https://api.themoviedb.org/3/movie/`
+        let data = {
+            external_id: req.params.movieID,
+            api_key: API,
+            language: "en-US",
+            // page: 1,
+            // include_adult: false
+        }
+        axios.get(url, {
+            params: data
+        }).then((response)=>{
             console.log(response.data.results);
             res.json(response.data.results);
         }).catch((err)=> {
             res.json(err);
         });
-    } 
+
+        // axios.get("https://api.themoviedb.org/3/movie/123025?api_key=80b6c8343cf908fcdc17ccb31170064b&language=en-US").then((response)=> {
+        //     res.json(response);
+        // }).catch((err)=> {
+        //     res.json(err);
+        // })
+    }
+
+
 }
--- routes/api/tmdb.js
+++ routes/api/tmdb.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
 const tmdbController = require("../../controllers/tmdbController");
 
-// Matches with "/api/tmdb/upcoming"
+// Matches with "/api/tmdb/search/upcoming"
 router
   .route("/search/upcoming")
   .get(tmdbController.findUpcoming);
 
+// router
+//   .route("/search/:searchType/:title")
+//   .get(tmdbController.searchTitle)
+
 router
-  .route("/search/movie/:movie")
-  .get(tmdbController.searchMovie)
+  .route("/search/movieDetails/:movieID")
+  .get(tmdbController.movieDetails)
 
 module.exports = router;
