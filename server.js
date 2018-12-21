1d8f3b050538ca8a02c79e8a3d11b6b35ddffddf
Evan Tucker <eej.tucker@gmail.com>
2018-12-21 11:41:21 -0700

    added if-env package

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const routes = require("./routes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
 app.use(routes);
 
 app.listen(PORT, (err)=> {
